933cc6f2a0509f8bbbd2b49f7c04efefb212feda
flnutmmyungju <mjmj01617@naver.com>
2019-05-28 11:20:23 +0900

    babel_update_set

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const express = require("express");
+import express from "express";
 const app = express();
 
 const PORT = 4500;
 
-const handleListening = () => {
-  console.log(`*1* listening on: http://localhost:${PORT}`);
-};
+const handleListening = () =>
+  console.log(`listening on: http://localhost:${PORT}`);
 
-const handleHome = (req, res) => {
-  console.log("*2* req");
-  res.send("hello from home");
-};
+const handleHome = (req, res) => res.send("hello from home");
+
+const handleProfile = (req, res) => res.send("you are on my profile");
 
 app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
 
 app.listen(PORT, handleListening);
