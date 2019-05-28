4460e31c28214267fb967919b09d8ab2d0803e4d
flnutmmyungju <mjmj01617@naver.com>
2019-05-28 13:09:40 +0900

    test_middleware_set

--- index.js
+++ index.js
@@ -4,13 +4,21 @@ const app = express();
 const PORT = 4500;
 
 const handleListening = () =>
-  console.log(`listening on: http://localhost:${PORT}`);
+  console.log(`listening on : http://localhost:${PORT}`);
 
 const handleHome = (req, res) => res.send("hello from home");
 
 const handleProfile = (req, res) => res.send("you are on my profile");
 
+const betweenHome = (req, res, next) => {
+  console.log("bettween");
+  next();
+};
+
+//route
 app.get("/", handleHome);
+
+app.use(betweenHome);
 
 app.get("/profile", handleProfile);
 
