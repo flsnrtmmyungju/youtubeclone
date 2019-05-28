f80ce1ff0b275d0c399fecab74d44795de427c6d
flnutmmyungju <mjmj01617@naver.com>
2019-05-28 10:33:28 +0900

    nodemon_set

--- index.js
+++ index.js
@@ -4,7 +4,14 @@ const app = express();
 const PORT = 4500;
 
 const handleListening = () => {
-  console.log(`listening on: http://localhost:${PORT}`);
+  console.log(`*1* listening on: http://localhost:${PORT}`);
 };
+
+const handleHome = (req, res) => {
+  console.log("*2* req");
+  res.send("hello from home");
+};
+
+app.get("/", handleHome);
 
 app.listen(PORT, handleListening);
