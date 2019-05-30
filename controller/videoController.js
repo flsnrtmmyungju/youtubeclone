3efb4ba31f441488526a69f4671a18123db39481
flnutmmyungju <mjmj01617@naver.com>
2019-05-30 15:34:18 +0900

    conntroller_modify

--- controller/videoController.js
+++ controller/videoController.js
@@ -1,9 +1,15 @@
 //videoController.js
 
-//                                   home는 pug템플릿에서 사용하기위해..
-export const home = (req, res) => res.render("home");
-export const search = (req, res) => res.send("Search");
-export const upload = (req, res) => res.send("Upload");
-export const videoDetail = (req, res) => res.send("Video Detail");
-export const editVideo = (req, res) => res.send("Edit Video");
-export const deleteVideo = (req, res) => res.send("Delete Video");
+//res.send : res.send("home");처럼 옵션한개만..
+//res.render : pug템플릿 각각에 정보를 전달하기위해.첫번째인자는 템플릿,두번째인자는 추가할정보가 담긴 객체
+export const home = (req, res) => res.render("home", { pageTitle: "Home" });
+export const search = (req, res) =>
+  res.render("search", { pageTitle: "Search" });
+export const upload = (req, res) =>
+  res.render("upload", { pageTitle: "Upload" });
+export const videoDetail = (req, res) =>
+  res.render("videoDetail", { pageTitle: "Video Detail" });
+export const editVideo = (req, res) =>
+  res.render("editVideo", { pageTitle: "Edit Video" });
+export const deleteVideo = (req, res) =>
+  res.render("deleteVideo", { pageTitle: "Delete Video" });
